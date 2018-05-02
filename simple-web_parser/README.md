d1098ac2c770447fc416b59f25de55fc29a987b5
ajboloor <adith94@gmail.com>
2018-05-02 14:25:26 -0500

    added readme for simple-web_parser

---
+++ simple-web_parser/README.md
@@ -0,0 +1,21 @@
+# simple-web_parser
+
+### Features:
+- Uses BeautifulSoup to parse the web
+- Example involves acquiring Github user's daily commit numbers
+
+### Requirements:
+- BeautifulSoup (https://www.crummy.com/software/BeautifulSoup/)
+```
+pip install beautifulsoup4
+```
+- matplotlib (https://matplotlib.org/)
+```
+pip install matplotlib
+```
+- seaborn [for plotting] (https://seaborn.pydata.org/)
+```
+pip install seaborn
+```
+
+
--- simple-web_parser/simple-web_parser.py
+++ simple-web_parser/simple-web_parser.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.colors import ListedColormap
 
 url = 'https://github.com/ajboloor'
 response = get(url)
@@ -21,5 +23,20 @@ for commits_day in commits:
 
 commits_matrix = np.array(commits_array[len(commits_array)-364:len(commits_array)],dtype=int)
 commits_matrix = commits_matrix.reshape(7,52)
-sns.heatmap(commits_matrix, cmap="GnBu", square=True)
+
+#myColors = ('#eee','#c6e48b','#7bc96f','#239a3b','#196127')
+myColors = ((0.8, 0.0, 0.0, 1.0), (0.0, 0.8, 0.0, 1.0), (0.0, 0.0, 0.8, 1.0), \
+	 (0.0, 0.0, 0.2, 1.0), (0.0, 0.2, 0, 1.0))
+#cmap = LinearSegmentedColormap.from_list('Custom', myColors, len(myColors))
+#cmap = sns.cubehelix_palette(start=2.8, rot=.1, light=0.9, n_colors=3)
+
+ax = sns.heatmap(commits_matrix, cmap='GnBu', square=True, \
+	 cbar_kws={"orientation": "horizontal"},linewidth=0.5)
+colorbar = ax.collections[0].colorbar
+#colorbar.set_ticks([0, 1, 5])
+#colorbar.set_ticklabels(['0','1+', '5+'])
+ax.set_ylabel('FROM')
+ax.set_xlabel('TO')
+_, labels = plt.yticks()
+plt.setp(labels, rotation=0)
 plt.show()
