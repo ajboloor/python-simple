c359ef0f1dc68b92e81173876c99f701c851d622
ajboloor <adith94@gmail.com>
2018-05-05 14:32:46 -0500

    converted helloworld to a function

--- simple-helloworld/simple-helloworld.py
+++ simple-helloworld/simple-helloworld.py
@@ -1,16 +1,19 @@
-print "HelloWorld!"
-print "\n"
+def HelloWorld():
+    print "HelloWorld!"
+    print "\n"
 
-name = raw_input("What is your name? ")
-print "Your name is " + name + ".\n"
+    name = raw_input("What is your name? ")
+    print "Your name is " + name + ".\n"
 
-a = 10
-b = 7
+    a = 10
+    b = 7
 
-print "a = " + str(a)
-print "b = " + str(b)
-print "a + b = " + str(a + b)
-print "a - b = " + str(a - b)
-print "a * b = " + str(a * b)
-print "a / b = " + str(float(a) / b)
-print "a ^ b = " + str(a ** b)
+    print "a = " + str(a)
+    print "b = " + str(b)
+    print "a + b = " + str(a + b)
+    print "a - b = " + str(a - b)
+    print "a * b = " + str(a * b)
+    print "a / b = " + str(float(a) / b)
+    print "a ^ b = " + str(a ** b)
+
+HelloWorld()
